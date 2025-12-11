8c43fde3bde8587a6885684f7adf7b1fc608bc3f
meek2100 <dtheurer@live.com>
2025-12-10 19:10:00 -0800

    Fix tests.

--- jest.setup.js
+++ jest.setup.js
@@ -4,17 +4,17 @@ const path = require("path");
 // We need to fetch and evaluate the source script to make its global functions available to JSDOM.
 
 // --- search.js Setup ---
-const searchJsPath = path.resolve(__dirname, "app/static/js/search.js");
+const searchJsPath = path.resolve(__dirname, "audiobook_automated/static/js/search.js");
 const searchJsContent = fs.readFileSync(searchJsPath, "utf8");
 global.searchJsContent = searchJsContent;
 
 // --- actions.js Setup ---
-const actionsJsPath = path.resolve(__dirname, "app/static/js/actions.js");
+const actionsJsPath = path.resolve(__dirname, "audiobook_automated/static/js/actions.js");
 const actionsJsContent = fs.readFileSync(actionsJsPath, "utf8");
 global.actionsJsContent = actionsJsContent;
 
 // --- status.js Setup (NEW) ---
-const statusJsPath = path.resolve(__dirname, "app/static/js/status.js");
+const statusJsPath = path.resolve(__dirname, "audiobook_automated/static/js/status.js");
 const statusJsContent = fs.readFileSync(statusJsPath, "utf8");
 global.statusJsContent = statusJsContent;
 // ------------------------------
